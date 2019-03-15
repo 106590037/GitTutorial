b31d1c2840f75f6884620166ca7c7179b532e8db
106590037 <48319951+106590037@users.noreply.github.com>
2019-03-15 11:51:42 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -10,13 +10,7 @@ bool Guess(int number) {
   Random r;
   target = r.Next() % 100 + 1;
   }
-  if (number == target) {
-  std::cout << "Correct !!";
-  target = -1;
-  return true;
-  }
-  else if (number > target) std::cout << "Smaller" << std::endl;
-  else if (number < target) std::cout << "Bigger" << std::endl;
+  if (number == target)return true;
   return false;
 }
 
@@ -24,8 +18,8 @@ int main(array<System::String ^> ^args)
 {
   int guess;
   do {
-    std::cout << "Choose a number between 1 - 100 :";
-    std::cin >> guess;
+		std::cout << "Choose a number between 1 - 100 :";
+		std::cin >> guess;
   } while (!Guess(guess));
   return 0;
 }
