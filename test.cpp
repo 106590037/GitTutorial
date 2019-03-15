c919005a4913d9fffebad058cd6dc50a886d5b52
106590037 <48319951+106590037@users.noreply.github.com>
2019-03-15 12:15:49 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -10,6 +10,37 @@ bool Guess(int number) {
   Random r;
   target = r.Next() % 100 + 1;
   }
+  if (number == target) {
+  std::cout << "Correct !!";
+  target = -1;
+  return true;
+  }
+  else if (number > target) std::cout << "Smaller" << std::endl;
+  else if (number < target) std::cout << "Bigger" << std::endl;
+  return false;
+}
+
+int main(array<System::String ^> ^args)
+{
+  int guess;
+  do {
+    std::cout << "Choose a number between 1 - 100 :";
+    std::cin >> guess;
+  } while (!Guess(guess));
+  return 0;
+}
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+
+using namespace System;
+
+bool Guess(int number) {
+  static int target = -1;
+  if (target == -1) {
+  Random r;
+  target = r.Next() % 100 + 1;
+  }
   if (number == target)return true;
   return false;
 }
